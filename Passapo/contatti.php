ce64157e61bba27d139fc56d9167b348306df6b0
d3athstorm <stefanomoro23@gmail.com>
2016-05-19 18:20:34 +0200

    scheletro completo

--- Passapo/contatti.php
+++ Passapo/contatti.php
@@ -11,24 +11,24 @@
 				<img alt="" src="img/immagine-testata-passapo.jpg">
 				<div id="pippo" style="position:relative;top:20%;">
 					<nav>
-						<ul class="topnav">
-						  <li><a href="default.asp">Home</a></li>
-						  <li class="parent"><a href="default.asp">Galleria</a>
+						<ul class="topnav" style="z-index: 0;">
+						  <li><a href="index.php">Home</a></li>
+						  <li class="parent"><a href="">Galleria</a>
 						      <ul class="child">
-						        <li><a href="#">Cerimonia</a></li>
-						        <li><a href="#">Autunno-Inverno</a></li>
-						        <li><a href="#">Primavera-Estate</a></li>
+						        <li><a href="galleria.php?categoria=cerimonia">Cerimonia</a></li>
+						        <li><a href="galleria.php?categoria=autinv">Autunno-Inverno</a></li>
+						        <li><a href="galleria.php?categoria=primest">Primavera-Estate</a></li>
 						      </ul>
 						  </li>
-						  <li class="parent"><a href="news.asp">Catalogo</a>
+						  <li class="parent"><a href="">Catalogo</a>
 						      <ul class="child">
-						        <li><a href="#">Cerimonia</a></li>
-						        <li><a href="#">Autunno-Inverno</a></li>
-						        <li><a href="#">Primavera-Estate</a></li>
+						        <li><a href="catalogo.php?categoria=cerimonia">Cerimonia</a></li>
+						        <li><a href="catalogo.php?categoria=autinv">Autunno-Inverno</a></li>
+						        <li><a href="catalogo.php?categoria=primest">Primavera-Estate</a></li>
 						      </ul>
 						  </li>
-						  <li><a href="contact.asp">Contatti</a></li>
-						  <li><a href="about.asp">Dove Siamo</a></li>
+						  <li><a href="contatti.php">Contatti</a></li>
+						  <li><a href="maps.php">Dove Siamo</a></li>
 						  <li class="icon">
 						    <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a>
 						  </li>
--- Passapo/index.php
+++ Passapo/index.php
@@ -12,23 +12,23 @@
 				<div id="pippo" style="position:relative;top:20%;">
 					<nav>
 						<ul class="topnav" style="z-index: 0;">
-						  <li><a href="default.asp">Home</a></li>
-						  <li class="parent"><a href="galleria.php">Galleria</a>
+						  <li><a href="index.php">Home</a></li>
+						  <li class="parent"><a href="">Galleria</a>
 						      <ul class="child">
 						        <li><a href="galleria.php?categoria=cerimonia">Cerimonia</a></li>
 						        <li><a href="galleria.php?categoria=autinv">Autunno-Inverno</a></li>
 						        <li><a href="galleria.php?categoria=primest">Primavera-Estate</a></li>
 						      </ul>
 						  </li>
-						  <li class="parent"><a href="news.asp">Catalogo</a>
+						  <li class="parent"><a href="">Catalogo</a>
 						      <ul class="child">
-						        <li><a href="#">Cerimonia</a></li>
-						        <li><a href="#">Autunno-Inverno</a></li>
-						        <li><a href="#">Primavera-Estate</a></li>
+						        <li><a href="catalogo.php?categoria=cerimonia">Cerimonia</a></li>
+						        <li><a href="catalogo.php?categoria=autinv">Autunno-Inverno</a></li>
+						        <li><a href="catalogo.php?categoria=primest">Primavera-Estate</a></li>
 						      </ul>
 						  </li>
-						  <li><a href="contact.asp">Contatti</a></li>
-						  <li><a href="about.asp">Dove Siamo</a></li>
+						  <li><a href="contatti.php">Contatti</a></li>
+						  <li><a href="maps.php">Dove Siamo</a></li>
 						  <li class="icon">
 						    <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a>
 						  </li>
@@ -37,7 +37,7 @@
 				</div>
 					<div id="presentation" style="position:relative;top:50px;width:100%;z-index:-2;">
 						<img alt="" src="img/Passapo-logo-grigio-150x150.png"><br><br>
-						<font>
+						<p class="text"><font>
 							La nostra azienda è specializzata nel settore accessori per<br>
 							bambini. Produciamo cerchietti, coroncine, cinturine, borsettine,<br>
 							mollettine. La lavorazione artigianale, ed i preziosi materiali sono le<br>
@@ -52,7 +52,7 @@
 							www.passapo.com<br>
 							facebook.com/passapomilano<br><br><br>
 							<b>PHOTOGRAPHER</b>/ Ruggero Farina. <b>DESIGNER</b>/ Francesca Giudici. <b>MODEL</b>/ Carolina.
-						</font>
+						</font></p>
 					</div>
 			</div>
 		</div>
