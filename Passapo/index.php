d5e4fa3e851548d5c6a8503779c78e72b42ab4d2
d3athstorm <stefanomoro23@gmail.com>
2016-05-19 13:02:54 +0200

    home terminata

--- Passapo/index.php
+++ Passapo/index.php
@@ -5,31 +5,55 @@
 		<link href="index.css" rel="stylesheet">
 	</head>
 	<body width="100%">
-		<div id="pippo">
-			<nav>
-				<ul class="topnav">
-				  <li><a href="default.asp">Home</a></li>
-				  <li class="parent"><a href="default.asp">Galleria</a>
-				      <ul class="child">
-				        <li><a href="#">Cerimonia</a></li>
-				        <li><a href="#">Autunno-Inverno</a></li>
-				        <li><a href="#">Primavera-Estate</a></li>
-				      </ul>
-				  </li>
-				  <li class="parent"><a href="news.asp">Catalogo</a>
-				      <ul class="child">
-				        <li><a href="#">Cerimonia</a></li>
-				        <li><a href="#">Autunno-Inverno</a></li>
-				        <li><a href="#">Primavera-Estate</a></li>
-				      </ul>
-				  </li>
-				  <li><a href="contact.asp">Contatti</a></li>
-				  <li><a href="about.asp">Dove Siamo</a></li>
-				  <li class="icon">
-				    <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a>
-				  </li>
-				</ul>
-			</nav>
+		<div id="logo" style="position:absolute;top:5%;width:100%;">
+			<img alt="" src="img/Passapo-logo-grigio-300x300.png">
+			<div id="testata" style="position:relative;top:5%;width:100%">
+				<img alt="" src="img/immagine-testata-passapo.jpg">
+				<div id="pippo" style="position:relative;top:20%;">
+					<nav>
+						<ul class="topnav">
+						  <li><a href="default.asp">Home</a></li>
+						  <li class="parent"><a href="default.asp">Galleria</a>
+						      <ul class="child">
+						        <li><a href="#">Cerimonia</a></li>
+						        <li><a href="#">Autunno-Inverno</a></li>
+						        <li><a href="#">Primavera-Estate</a></li>
+						      </ul>
+						  </li>
+						  <li class="parent"><a href="news.asp">Catalogo</a>
+						      <ul class="child">
+						        <li><a href="#">Cerimonia</a></li>
+						        <li><a href="#">Autunno-Inverno</a></li>
+						        <li><a href="#">Primavera-Estate</a></li>
+						      </ul>
+						  </li>
+						  <li><a href="contact.asp">Contatti</a></li>
+						  <li><a href="about.asp">Dove Siamo</a></li>
+						  <li class="icon">
+						    <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a>
+						  </li>
+						</ul>
+					</nav>
+					<div id="presentation" style="position:relative;top:30;width:100%">
+						<br><br><br><br><br><br>
+						<img alt="" src="img/Passapo-logo-grigio-150x150.png"><br><br>
+						La nostra azienda è specializzata nel settore accessori per<br>
+						bambini. Produciamo cerchietti, coroncine, cinturine, borsettine,<br>
+						mollettine. La lavorazione artigianale, ed i preziosi materiali sono le<br>
+						principali caratteristiche, capaci di trasformare un semplice cerchietto<br>
+						in una vera e propria opera d’arte.<br><br><br><br>
+						Our company specializes in accessories for children. We produce<br>
+						headbands, tiaras, belts, purses, hairclips. The handmade<br>
+						workmanship and the precious materials are the main features, that<br>
+						enable us to transform a simple headband into a real work of art.<br><br><br><br><br>
+						+39 335 8010339<br>
+						passapomilano@gmail.com / info@passapo.com<br>
+						www.passapo.com<br>
+						facebook.com/passapomilano<br><br><br>
+						<b>PHOTOGRAPHER</b>/ Ruggero Farina. <b>DESIGNER</b>/ Francesca Giudici. <b>MODEL</b>/ Carolina.
+					</div>
+				</div>
+			</div>
 		</div>
 		<script>
 		function myFunction() {
@@ -40,6 +64,5 @@
 </html>
 
 <?php
-	echo "ancora più banane";
-	echo "basta troppe banane";
+
 ?>
