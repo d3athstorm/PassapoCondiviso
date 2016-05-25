fe0174c9daf0604fb8fe934834426d980fbf8d87
d3athstorm <stefanomoro23@gmail.com>
2016-05-25 20:16:36 +0200

    più o meno finita la sessione mappa

--- Passapo/maps.php
+++ Passapo/maps.php
@@ -3,6 +3,33 @@
 	<head>
 		<title></title>
 		<link href="index.css" rel="stylesheet">
+		<script src="http://maps.googleapis.com/maps/api/js"></script>
+		<script>
+			var myCenter=new google.maps.LatLng(45.4582682,9.1516000);
+			var marker;
+			
+			function initialize() {
+			  var mapProp = {
+			    center:myCenter,
+			    zoom:17,
+			    mapTypeId:google.maps.MapTypeId.ROADMAP
+			  };
+			  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+			  
+			  var marker=new google.maps.Marker({
+				  position:myCenter,
+				  animation:google.maps.Animation.BOUNCE
+				  });
+
+				marker.setMap(map);
+				var infowindow = new google.maps.InfoWindow({
+					  content:"Passapò Milano"
+					  });
+
+					infowindow.open(map,marker);
+			}
+			google.maps.event.addDomListener(window, 'load', initialize);
+		</script>
 	</head>
 	<body>
 		<div id="logo" style="position:absolute;top:5%;width:100%;">
@@ -35,6 +62,18 @@
 						</ul>
 					</nav>
 				</div>
+					<div id="indirizzo" style="float:left;position:relative;top:100px;width:50%;z-index:1;">
+						<p>
+							Lo Showroom lo puoi trovare in:<br>
+						</p>
+						<p class="ind">
+							<font>
+								Via Garian 51, Milano(MI)
+							</font>
+						</p>
+					</div>
+					<div id="googleMap" style="float:right;position:relative;width:50%;height:800px;z-index:1;">	
+					</div>
 			</div>
 		</div>
 		<script>
